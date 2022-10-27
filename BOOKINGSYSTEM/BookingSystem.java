9ecd047040a6b83a93475937b9303587dc7f776a
Oliver Nagy <19269749@studentmail.ul.ie>
2022-10-27 15:28:27 +0100

    BookingSystem fix

--- BOOKINGSYSTEM/BookingSystem.java
+++ BOOKINGSYSTEM/BookingSystem.java
@@ -8,8 +8,6 @@ public class BookingSystem {
 
     }
 
-    public void makeBoooking(String username, LocalDate checkIn, LocalDate checkOut, String location, String roomType,
-            String nbOfGuests, int uniqueReservationNumber) {
-    }
+   
 
 }
