5e86ef49ff77d6f65d87073a797f9413e431866c
emmetbrowne <18238637@studentmail.ul.ie>
2022-11-23 14:38:53 +0000

    created observer folder

---
+++ User/Observer/Channels.java
@@ -0,0 +1,63 @@
+/*
+ * 
+ * Author - Emmet
+ * This class is for our activities. This is where activities are created.
+ * 
+ * 
+ */
+
+package USER.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Channels {
+
+    public List<Subscriber> subs = new ArrayList<>(); // the list of subscribers
+    public String title; // the title of the activiy, ie: yoga class, zoomba
+    public String dateTime; // the time and date of the activity
+
+    /*
+     * Adds the details of which subscriber has subscribed to subscribers
+     * Attaches user to sub, they will now receive notifications for each class
+     * added
+     */
+
+    public void attach(Subscriber sub) {
+
+        subs.add(sub);
+
+    }
+
+    /*
+     * Removes the details of which subscriber has unsubscribed
+     * Detaches subscriber from channel, they will no longer receive notificaitons
+     */
+
+    public void detach(Subscriber sub) {
+        subs.remove(sub);
+    }
+
+    /*
+     * Sends update to all of the subscribers
+     * 
+     */
+
+    public void notifySubscribers() {
+
+        for (Subscriber sub : subs) {
+            sub.update();
+        }
+
+    }
+
+    /*
+     * Assigns the details of the activity
+     */
+    public void updateActivities() {
+
+        this.title = title;
+        this.dateTime = dateTime;
+        notifySubscribers();
+    }
+}
