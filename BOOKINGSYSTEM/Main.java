5b59049360140889ba879020b6dcf2f1a965a2cb
Oliver Nagy <19269749@studentmail.ul.ie>
2022-11-23 14:41:56 +0000

    fix

--- BOOKINGSYSTEM/Main.java
+++ BOOKINGSYSTEM/Main.java
@@ -1,12 +1,16 @@
 package BOOKINGSYSTEM;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import GUI.AdminUI;
 import GUI.UserUI;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
 
-        UserUI.Create();
+        AdminUI.Create();
     }
 
 }
