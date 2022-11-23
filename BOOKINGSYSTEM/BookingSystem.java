277a8555a557da2b32005d81e2b9dab1655b1edb
Oliver Nagy <19269749@studentmail.ul.ie>
2022-11-23 17:34:49 +0000

    FactoryFix

--- BOOKINGSYSTEM/BookingSystem.java
+++ BOOKINGSYSTEM/BookingSystem.java
@@ -1,8 +1,24 @@
 package BOOKINGSYSTEM;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import GUI.Factory.GUI;
+import GUI.Factory.GUIFactory;
+import GUI.Factory.UserUI;
+
 public class BookingSystem {
 
-    public BookingSystem() {
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        // Factory
+        GUIFactory gui = new GUIFactory();
+        GUI login = gui.getGUI("Login");
+        try {
+            login.Create();
+        } catch (IOException e1) {
+            // TODO Auto-generated catch block
+            e1.printStackTrace();
+        }
 
     }
 
