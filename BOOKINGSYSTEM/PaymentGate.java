28e961b93de4b9a18b587bca3a05f4530e032d03
emmetbrowne <18238637@studentmail.ul.ie>
2022-11-28 16:16:17 +0000

    stripe implementation

---
+++ BOOKINGSYSTEM/PaymentGate.java
@@ -0,0 +1,13 @@
+package BOOKINGSYSTEM;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import GUI.Factory.MembershipUI;
+import GUI.Factory.STATEDESIGN.GUIContext;
+import GUI.Factory.STATEDESIGN.StartLoginGUI;
+import GUI.Factory.STATEDESIGN.State;
+
+public class PaymentGate {
+
+}
